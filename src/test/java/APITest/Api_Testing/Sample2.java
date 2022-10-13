7c04be72095f52b0fc0b42f5ca71a21635142a14
ChinniahVenkatesh <chinniahjudo27@gmail.com>
2022-10-13 22:16:20 +0530

    Fist changes to develop

---
+++ src/test/java/APITest/Api_Testing/Sample2.java
@@ -0,0 +1,11 @@
+package APITest.Api_Testing;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+
+	}
+
+}
